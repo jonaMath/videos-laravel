e28b3b5f2bd3bed482958ab6b2a32644515ef0d8
JonathanG <gb18006@ues.edu.sv>
2020-02-09 15:20:35 -0600

    version 9-feb-20 DB troubble

---
+++ database/migrations/2020_02_07_165157_videos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class VideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->bigIncrements('id', 255);
+            $table->integer('user_id', 255);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('status', 20);
+            $table->string('image', 255);
+            $table->string('video_path', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2020_02_09_211036_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->bigIncrements('id', 255);
+            $table->integer('user_id', 255);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('video_id', 255);
+            $table->foreign('video_id')->references('id')->on('videos');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+}
