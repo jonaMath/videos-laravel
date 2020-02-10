e89c5b9cfe7c4ff91879219849b0cbadb2a096e1
JonathanG <gb18006@ues.edu.sv>
2020-02-10 10:51:45 -0600

    errors in videoController

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Videos Platform') }}</title>
-
     <!-- Styles -->
     <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{URL::asset('css/style.css')}}">
+    
 </head>
 <body>
     <div id="app">
